03281d131a66bae3824cdf390c4aa87f3efe26c4
Maksym Wołoszynowski <m.woloszynowski.272@studms.ug.edu.pl>
2025-01-30 20:28:13 +0100

    commit

---
+++ graphics/tiles/Zombie-Tileset---_0159_Capa-160.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Zombie-Tileset---_0159_Capa-160" tilewidth="12" tileheight="16" tilecount="1" columns="1">
+ <image source="../../../../../../Downloads/Zombie Apocalypse Tileset/Organized separated sprites/Urban Assets/Zombie-Tileset---_0159_Capa-160.png" width="12" height="16"/>
+</tileset>
---
+++ graphics/tiles/output-onlinepngtools-2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="output-onlinepngtools-2" tilewidth="51" tileheight="51" tilecount="264" columns="22">
+ <image source="../../../../../../Downloads/output-onlinepngtools-2.png" width="1127" height="613"/>
+</tileset>
